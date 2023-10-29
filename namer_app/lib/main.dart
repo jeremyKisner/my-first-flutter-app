c59d8446f383880f981c14fdb0f9027f73b75f6d
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-10-28 23:43:18 -0400

    added functionality

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -31,6 +31,17 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -39,6 +50,12 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
     return Scaffold(
       body: Center(
         child: Column(
@@ -46,12 +63,25 @@ class MyHomePage extends StatelessWidget {
           children: [
             BigCard(pair: pair),
             SizedBox(height: 14.00),
-            ElevatedButton(
-              onPressed: () {
-                print('button pressed!');
-                appState.getNext();
-              },
-              child: Text('NEXT'),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    print('LIKE button pressed!');
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('LIKE'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('NEXT button pressed!');
+                    appState.getNext();
+                  },
+                  child: Text('NEXT'),
+                ),
+              ],
             ),
           ],
         ),
